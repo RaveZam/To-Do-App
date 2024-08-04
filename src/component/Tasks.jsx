b4142797c734b2f0c18f2a6bb68e4a073259093b
Duke <114975455+RaveZam@users.noreply.github.com>
2024-08-04 12:34:16 +0800

    Finished

--- src/component/Tasks.jsx
+++ src/component/Tasks.jsx
@@ -3,12 +3,10 @@ import Totaltasks from "./TotalTasks";
 import Completedtasks from "./CompletedTasks";
 export default function Tasks({ totalTodos, completedTodos }) {
   return (
-    <div>
-      <h2 className={styles.taskslabel}>Total Tasks</h2>
-      <div className={styles.tasksContainer}>
-        <Totaltasks totalTodos={totalTodos} />
-        <Completedtasks completedTodos={completedTodos} />
-      </div>
+    <div className={styles.tasksContainer}>
+      <h2 className={styles.taskslabel}>Total Tasks:</h2>
+      <Totaltasks totalTodos={totalTodos} />
+      <Completedtasks completedTodos={completedTodos} />
     </div>
   );
 }
